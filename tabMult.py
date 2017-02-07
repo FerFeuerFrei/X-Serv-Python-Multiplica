18a814e8b1e9067fd3c42c87ab3e2050c60fe38f
FerFeuerFrei <f.torrijos@alumnos.urjc.es>
2017-02-07 14:07:37 +0100

    Tabla de multiplicar

--- tabMult.py
+++ tabMult.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 31 10:03:56 2017
 
-@author: ftorrijo
 """
 
+Tabla de multiplicar
+
+"""
+
+# Rango de 1 a 10
 rango = range(1,11)
+ 
+# Ahora imprimimos desde la tabla de multiplicar del 1 hasta la
+# tabla de multiplicar del 10
+
 for num in rango:
-    print "\nTabla del " + str(num) + '\n' + '############'
+    print "\nTabla del " + str(num) + '\n' + ''
     for num2 in rango:
-        print str(num) + ' x ' + str(num2) + ' = ' + str(num*num2)
+        print str(num) + 'x' + str(num2) + '=' + str(num*num2)
